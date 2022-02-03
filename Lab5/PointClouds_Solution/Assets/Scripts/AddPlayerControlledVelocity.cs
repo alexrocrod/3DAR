6959afbfaf5b8625c874dc6cd2bd002fd431defb
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-02-03 16:11:19 +0100

    cube as a car and camera follows

---
+++ Lab5/PointClouds_Solution/Assets/Scripts/AddPlayerControlledVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+    // Add a force vector
+    [SerializeField]
+    Vector3 v3Force;
+
+    // Variables to select the keys to get when the force is applied
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
